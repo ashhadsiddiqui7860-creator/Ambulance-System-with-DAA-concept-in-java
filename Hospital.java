3411052707f3847f8c8fab50bea6c5378508803c
ashhadsiddiqui7860-creator <ashhadsiddiqui7860@gmail.com>
2026-04-21 11:28:43 +0530

    Update Hospital.java

--- Hospital.java
+++ Hospital.java
@@ -8,4 +8,12 @@ public class Hospital {
         this.name = name;
         this.location = location;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
 }
