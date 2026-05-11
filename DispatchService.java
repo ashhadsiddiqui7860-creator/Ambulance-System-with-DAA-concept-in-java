142b46da612b0dec58bd58aed9fa650366afa9e4
ashhadsiddiqui7860-creator <ashhadsiddiqui7860@gmail.com>
2026-05-11 14:47:38 +0530

    Update DispatchService.java

--- DispatchService.java
+++ DispatchService.java
@@ -2,29 +2,35 @@ package ambsys;
 
 import java.util.Scanner;
 
-public class DispatchService {
+public class DispatchService 
+{
     private Scanner sc = new Scanner(System.in);
     private Graph graph;
     private AmbulanceService ambulanceService;
 
-    public DispatchService(Graph graph, AmbulanceService ambulanceService) {
+    public DispatchService(Graph graph, AmbulanceService ambulanceService) 
+    {
         this.graph = graph;
         this.ambulanceService = ambulanceService;
     }
 
-    public void dispatch() {
+    public void dispatch() 
+    {
         System.out.print("Enter Emergency Location: ");
         String location = sc.nextLine().toLowerCase();
 
         Ambulance amb = ambulanceService.findNearest(location, graph);
 
-        if (amb != null) {
+        if (amb != null) 
+        {
             ambulanceService.updateAvailability(amb.getId(), false);
 
             System.out.println("Ambulance " + amb.getId() +
                     " dispatched from " + amb.getCurrentLocation() +
                     " to " + location);
-        } else {
+        } 
+        else
+        {
             System.out.println("No ambulance available.");
         }
     }
